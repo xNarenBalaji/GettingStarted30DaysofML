82dec36b38bb4f1358fd528caaeb117678c87f2c
NarenBalaji <vnarenbalaji@gmail.com>
2021-08-18 13:46:41 +0200

    Functions defined - Scores unchanged

--- getting_started_comp.py
+++ getting_started_comp.py
@@ -33,77 +33,91 @@ from xgboost import XGBRegressor
 # =============================================================================
 # Reading Data
 # =============================================================================
-
-pathlocation = 'Not Chosen'
-pathlocation = input("Location of dataset (home or away): ")
-if pathlocation.lower() == 'home':
-    base_path = "C:\\Users\\NarenBalaji\\JB\\ML\\Kaggle\\GettingStarted30DaysofML"
-elif pathlocation.lower() == 'away':
-    base_path ="D:\\Naren Balaji\\External Data\\Kaggle\\ml_30days"
-else:
-    print("Please choose correct location")
-    sys.exit()
-print(base_path)
-
-now = datetime.now()
-current_time = now.strftime("%H:%M:%S")
-print("Current Time =", current_time)
-
-train = pd.read_csv("train.csv",index_col='id')
-test = pd.read_csv("test.csv",index_col='id')
+def TrainLocation():
+    
+    pathlocation = 'Not Chosen'
+    pathlocation = input("Location of dataset (home or away): ")
+    if pathlocation.lower() == 'home':
+        base_path = "C:\\Users\\NarenBalaji\\JB\\ML\\Kaggle\\GettingStarted30DaysofML"
+    elif pathlocation.lower() == 'away':
+        base_path ="D:\\Naren Balaji\\External Data\\Kaggle\\ml_30days"
+    elif pathlocation.lower() == 'kaggle':
+        base_path = ""
+    else:
+        print("Please choose correct location")
+        sys.exit()
+    print(base_path)
+    
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    print("Current Time =", current_time)
+    
+    train = pd.read_csv(base_path+"\\"+"train.csv",index_col='id')
+    test = pd.read_csv(base_path+"\\"+"test.csv",index_col='id')
+    
+    return [train,test]
 
 # =============================================================================
 # Segregating data
 # Training target values in 'y'
 # Training feature values in 'features'
 # =============================================================================
-pd.set_option('display.max_columns', None)
-train.head()
+def DataSegregation(train,test):
+    #train.isna().any() #Checking for missing values
+    train = train.dropna(axis=0) 
+    y_train = train['target']
+    X_full = train.drop(['target'],axis=1)
+    X_full.head()
+    X_test = test
 
-y = train['target']
-features = train.drop(['target'],axis=1)
-features.head()
+    # Checking for missing values
+    # cols_with_missing = [col for col in X_full.columns if X_full[col].isnull().any()]
+    # No missing values
+    
+    # Separating categorical and numeric features
+    obj_cols = [col for col in X_full.columns if 'cat' in col]
+    num_cols = [col for col in X_full.columns if 'cont' in col]
+    
+    return [X_full, y_train, X_test, obj_cols, num_cols]
+    
 
-# Checking for missing values
-cols_with_missing = [col for col in features.columns
-                     if features[col].isnull().any()]
-# No missing values
+def DataProcessorChoice(num_cols, obj_cols, choice=0):
+    
+    if choice == 1:
+        num_transformer = SimpleImputer(strategy='mean')
+        cat_transformer = Pipeline(
+            steps=[('imputer', SimpleImputer(strategy='most_frequent'))
+                   ,('ordinal',OrdinalEncoder(handle_unknown='error'))
+                   ])
+        preprocessor = ColumnTransformer(transformers=
+                                         [('cont', num_transformer, num_cols)
+                                          ,('cat',cat_transformer, obj_cols)
+                                          ])
+    else: 
+        num_transformer = SimpleImputer(strategy='mean')
+        cat_transformer = Pipeline(
+            steps=[('imputer', SimpleImputer(strategy='most_frequent'))
+                   ,('onehot',OneHotEncoder(handle_unknown='ignore'))
+                   ])
+        preprocessor = ColumnTransformer(transformers=
+                                         [('cont', num_transformer, num_cols)
+                                          ,('cat',cat_transformer, obj_cols)
+                                          ])
+        
+    return preprocessor
+    
 
-# Separating categorical and numeric features
-object_cols = [col for col in features.columns if 'cat' in col]
-numeric_cols = [col for col in features.columns if 'cont' in col]
-
-
-# =============================================================================
-# Seaborn EDA
-# =============================================================================
-#sns.lineplot(data=features[numeric_cols[1]])
-#features[numeric_cols].describe()
-#sns.scatterplot(x=features.cont9,y=features.cont10)
-
-
-# =============================================================================
-# Train_test_split
-# =============================================================================
-#X_train, X_valid, y_train, y_valid = train_test_split(features, y, train_size)
-
-
-
+def ModelChoice(choice=0):
+    
+    if choice==1:
+        print("do what?")
+    else:
+        model = XGBRegressor(n_estimators=200,random_state=1)
+        
+    return model
 # =============================================================================
 # Imputing for missing values, however train/valid/test do not have any
 # =============================================================================
-numerical_transformer = SimpleImputer(strategy='mean')
-
-categorical_transformer = Pipeline(steps=[
-    ('imputer', SimpleImputer(strategy='most_frequent')),
-    ('onehot', OneHotEncoder(handle_unknown='ignore'))
-])
-
-preprocessor = ColumnTransformer(
-    transformers=[
-        ('num', numerical_transformer, numeric_cols),
-        ('cat', categorical_transformer, object_cols)
-    ])
 
 def model_choice(i,n_estimators=10):
     
@@ -115,7 +129,7 @@ def model_choice(i,n_estimators=10):
         model = RandomForestRegressor(n_estimators=100, random_state=1)
         my_pipeline = Pipeline(steps=[('preprocessor',preprocessor),
                                       ('model',model)])
-        scores = -1 * cross_val_score(my_pipeline, features, y, 
+        scores = -1 * cross_val_score(my_pipeline, X_full, y_train, 
                                       cv=5, n_jobs = -1, 
                                       scoring='neg_root_mean_squared_error')
     
@@ -124,14 +138,14 @@ def model_choice(i,n_estimators=10):
                              early_stopping_rounds=10)
         my_pipeline = Pipeline(steps=[('preprocessor',preprocessor),
                                       ('model',model)])
-        scores = -1 * cross_val_score(my_pipeline, features, y, 
+        scores = -1 * cross_val_score(my_pipeline, X_full, y_train, 
                                       cv=5, n_jobs = -1,
                                       scoring='neg_root_mean_squared_error')
     else:
         model = RandomForestRegressor(n_estimators=50, random_state=1)
         my_pipeline = Pipeline(steps=[('preprocessor',preprocessor),
                                       ('model',model)])
-        scores = -1 * cross_val_score(my_pipeline, features, y, 
+        scores = -1 * cross_val_score(my_pipeline, X_full, y_train, 
                                       cv=5, n_jobs = -1, 
                                       scoring='neg_root_mean_squared_error')
     
@@ -142,38 +156,62 @@ def model_choice(i,n_estimators=10):
     print(scores.mean())
     return scores.mean()
 
+#def ModelFit(model, preprocessor):
+    
+    
 # =============================================================================
 # Training model
 # =============================================================================
-model_choice(2,200)
 
-model = XGBRegressor(n_estimators=200,random_state=1,
-                     early_stopping_rounds=10)
-model.fit(features,y)
 
-# =============================================================================
-# numerical_transformer = SimpleImputer(strategy='mean')
-# 
-# categorical_transformer = Pipeline(steps=[
-#     ('imputer', SimpleImputer(strategy='most_frequent')),
-#     ('onehot', OneHotEncoder(handle_unknown='ignore'))
-# ])
-# 
-# preprocessor = ColumnTransformer(
-#     transformers=[
-#         ('num', numerical_transformer, numeric_cols),
-#         ('cat', categorical_transformer, object_cols)
-#     ])
-# my_pipeline = Pipeline(steps=[('preprocessor',preprocessor)])
-#                       
-#                        
+if __name__ == "__main__":
+    [train,test] = TrainLocation()
+    [X_full, y_train, X_test, 
+     obj_cols, num_cols] =  DataSegregation(train, test)
+    preprocessor = DataProcessorChoice(num_cols, obj_cols, choice=1)
+    model = ModelChoice(choice=0)
+    
+    pipe = Pipeline([
+        ('preprocessor',preprocessor), 
+        ('model', model)
+        ])
+    
+    pipe.fit(X_full,y_train)
+    
+    y_train_pred = pipe.predict(X_full)
+    
+    print(f"Predictions on training data: {y_train_pred}")
+    
+    mean_squared_error(y_train, y_train_pred, squared=False)
+    
+    y_test_pred = pipe.predict(X_test)
+    
+    print(f"Predictions on test data: {y_test_pred}")
+    
+
 # =============================================================================
 
 # Use the model to generate predictions
-predictions = model.predict(test)
+# predictions = my_pipeline.predict(test)
 
-# Save the predictions to a CSV file
-output = pd.DataFrame({'Id': test.index,
-                       'target': predictions})
-output.to_csv('submission.csv', index=False)
+
+#### To submit predictions
+
+# =============================================================================
+# # Save the predictions to a CSV file
+# output = pd.DataFrame({'Id': test.index,
+#                        'target': y_test_pred})
+# output.to_csv('submission.csv', index=False)
+# =============================================================================
     
+#### Unused code
+
+# =============================================================================
+# Seaborn EDA
+# =============================================================================
+#sns.lineplot(data=features[numeric_cols[1]])
+#features[numeric_cols].describe()
+#sns.scatterplot(x=features.cont9,y=features.cont10)
+
+
+
